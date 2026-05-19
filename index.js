6be91dfda5245ad5c5c378fa5f8c91083331a561
Md Towfiqur Rahman <140777602+towfiqurv360@users.noreply.github.com>
2026-05-19 11:39:10 +0600

    feat: add GET API for single tutor details

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -33,6 +33,12 @@ async function run() {
     app.get('/tutors', async (req, res) => {
       const cursor = tutorsCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get('/tutors/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await tutorsCollection.findOne(query);
       res.send(result);
     });
 
